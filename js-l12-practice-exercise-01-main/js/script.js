cd7da355812f42bfe409ca85306b35d515bc005d
tiff3b <tiffany3berger@gmail.com>
2025-06-18 17:52:32 -0400

    Added dropdown list

--- js-l12-practice-exercise-01-main/js/script.js
+++ js-l12-practice-exercise-01-main/js/script.js
@@ -1,13 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
+const hiddenUsers = document.querySelector(".num-users");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function (){
-    const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+const getData = async function (numUsers){
+    //modify API to use numUsers for the results
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     //find array with the data you want to use
     const userResults = data.results;
     displayUsers(userResults);
 };
-getData();
+//add 1 for the first argument to the getData
+getData(1);
 
 const displayUsers = function(userResults){
     //empty element so you don't get duplicates
@@ -25,5 +29,12 @@ const displayUsers = function(userResults){
        <p> ${country}</p>
        <img src=${imageUrl} alt="User avatar" />`;
        randomFolks.append(userDiv);
+       //add the drop down box
+       hiddenUsers.classList.remove("hide");
     }
 };
+
+selectUserNumber.addEventListener("change", function(e){
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
